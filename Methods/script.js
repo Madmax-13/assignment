e5ee48ec0b1f9de0fdd75ddd29d6b1a88e350cef
madmax-13 <chidiebubemaxwell13@gmail.com>
2024-10-14 03:05:42 +0100

    commiting the array methods task

---
+++ Methods/script.js
@@ -0,0 +1,71 @@
+//Assignment 
+//1: 10 array methods
+let x;
+const arr = [1,2,3,4,5,6];
+const arr1 = [11,12,13,14,15];
+//arr.push(7);
+//arr.pop();
+//arr.unshift();
+//arr.shift();
+//arr.reverse();
+//x = arr.indexOf(2);
+//arr.includes();
+//x = arr.slice(1, 5);
+//x = arr.splice(2, 4);
+//x = arr.concat(arr1);
+
+//console.log(x);
+
+//2: 10 String methods
+let y;
+const str = 'My name is Wisdom Maxwell';
+//y = str.length;
+//y = str.includes("name");
+//y = str.startsWith("wisdom");
+//y = str.endsWith("Maxwell");
+//y = str.indexOf("is");
+//y = str.toLowerCase();
+//y = str.toUpperCase();
+//y = str.replace("Wisdom", "Ebube");
+//y = str.slice(1, 12);
+//y = str.split(",");
+
+//console.log(y);
+
+//3: 5 Number methods
+let z;
+const num = 230;
+//z = num.toString();
+//z = num.toExponential(2);
+//z = num.toFixed(0);
+//z = num.toPrecision(2);
+//z = num.valueOf();
+
+//console.log(z);
+
+//4: 5 Math method
+let a;
+//a = Math.pow(5, 4);
+//a = Math.sqrt(28);
+//a = Math.min(20, 10, 50, 5, 70);
+//a = Math.max(20, 10, 5, 50, 75);
+//a = Math.random() * 10 ;
+
+//console.log(a);
+
+//5: 5 Object methods
+//let b;
+/*const person1 = {
+    firstName: "Wisdom",
+    lastName: "Maxwell",
+    age: 24,
+    skinColor: "brown"
+};*/
+//const person2 = {firstName: "Wisdom",lastName: "Maxwell",};
+//b = Object.assign(person1, person2);
+//b = Object.entries(person1);
+//b = Object.values(person1);
+//b = Object.keys(person1);
+//b = Object.create(person1);
+
+//console.log(b);
